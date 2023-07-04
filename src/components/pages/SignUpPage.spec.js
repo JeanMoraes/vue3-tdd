c06aea48a611a11081da8bf56cb2c400c599cb8a
Jean Moraes <jean.moraes@t10.digital>
2023-07-03 23:36:45 -0300

    test: testes do formulário de cadastro

--- src/components/pages/SignUpPage.spec.js
+++ src/components/pages/SignUpPage.spec.js
@@ -2,8 +2,49 @@ import SignUpPage from "./SignUpPage.vue";
 import { render, screen } from "@testing-library/vue";
 import "@testing-library/jest-dom"
 
-it("teste ajh", () => {
-    render(SignUpPage);
-    const header = screen.queryByRole("heading", { name: "Sign Up"});
-    expect(header).toBeInTheDocument()
-});
+describe("SignUp Page", () => {
+    describe("Layout", () => {
+        it("Testando o Header", () => {
+            render(SignUpPage);
+            const header = screen.queryByRole("heading", { name: "Sign Up"});
+            expect(header).toBeInTheDocument()
+        });
+
+        it("Testando campo de usuário", () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText("Username");
+            expect(input).toBeInTheDocument()
+        });
+
+        it("Testando campo de email", () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText("E-mail");
+            expect(input).toBeInTheDocument()
+        });
+
+        it("Testando campo de senha", () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText("Password");
+            expect(input).toBeInTheDocument()
+        });
+
+        it("Testando campo de repetir a senha", () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText("Password Repeat");
+            expect(input).toBeInTheDocument()
+        });
+
+        it("Testando o botão de enviar", () => {
+            render(SignUpPage);
+            const button = screen.queryByRole("button", { name: "Sign Up"});
+            expect(button).toBeInTheDocument()
+        });
+
+        it("Testando se o botão de enviar inicia desativado", () => {
+            render(SignUpPage);
+            const button = screen.queryByRole("button", { name: "Sign Up"});
+            expect(button).toBeDisabled()
+        });
+
+    })
+})
