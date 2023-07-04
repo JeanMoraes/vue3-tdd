05d75fd8fcf4c6ec53855de606a26d61bdbf7b55
Jean Moraes <jean.moraes@t10.digital>
2023-07-03 23:36:25 -0300

    feat: adicionando campos do formulário de cadastro

--- src/components/pages/SignUpPage.vue
+++ src/components/pages/SignUpPage.vue
@@ -1,3 +1,18 @@
 <template>
     <h1>Sign Up</h1>
+
+    <label for="username">Username</label>
+    <input id="username" />
+
+    <label for="e-mail">E-mail</label>
+    <input id="e-mail" />
+
+    <label for="password">Password</label>
+    <input id="password" />
+
+    <label for="password-repeat">Password Repeat</label>
+    <input id="password-repeat" type="password" />
+
+    <button disabled>Sign Up</button>
+
 </template>
