0ad0674604421d1b74b8d1839594df44d410c6e3
Jean Moraes <jean.moraes@t10.digital>
2023-08-18 17:17:49 -0300

    feat: encryptando o localstorage

--- src/state/storage.js
+++ src/state/storage.js
@@ -1,19 +1,24 @@
+import SecureLS from "secure-ls"
+
+const secureLS = new SecureLS()
+
 const setItem = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value))
+    secureLS.set(key, value)
+    // localStorage.setItem(key, JSON.stringify(value))
 }
 
 const getItem = (key) => {
-    const stored = localStorage.getItem(key)
+    return secureLS.get(key)
+    // const stored = localStorage.getItem(key)
+    // if(!stored) {
+    //     return null
+    // }
 
-    if(!stored) {
-        return null
-    }
-
-    try {
-        return JSON.parse(stored)
-    } catch {
-        return stored
-    }
+    // try {
+    //     return JSON.parse(stored)
+    // } catch {
+    //     return stored
+    // }
 }
 
 const clear = () => {
